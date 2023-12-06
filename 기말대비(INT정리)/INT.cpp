f85fc579639efb839578a73cec85f4ff1840ff76
a9297 <a9297@>
2023-12-06 17:09:03 +0900

    초기 커밋.

---
+++ 기말대비(INT정리)/INT.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+// ++, ()
+//int 를 표현하는 class 만들기
+class INT {
+	int val;
+public:
+	INT() = default;
+	INT(int v) : val{ v } {}
+
+	INT& operator++() {		//전위 증가
+		++val;
+		return*this;
+	}
+
+	INT& operator++(int) {
+		INT temp{ *this };
+		++(*this);
+		return temp;
+	}		// n ++ ++ ++ 이게 안됨 왜 안될까?
+
+
+
+	friend ostream operator<<(ostream& os,  INT s) {
+		return os << s;
+	}
+};
